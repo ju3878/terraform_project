7ec1680e321f48ac29ff525285af7bd7851e48ec
ju3878 <ju3878@gmail.com>
2024-02-07 15:32:22 +0900

    2024-02-07

---
+++ 01_statement/05_for/main.tf
@@ -0,0 +1,20 @@
+provider "local" { 
+} 
+
+variable "names" { 
+    description = "A map of names" 
+    # description = "A list of names" 
+    # type        = list(string) 
+    # default     = ["neo", "trinity", "morpheus"]
+    type = map(string)
+    default = {
+      neo = "hero"
+      trinity = "love interest"
+      morpheus = "mentor"
+    }
+} 
+
+output "upper_names" { 
+    # value = [ for name, role in var.names : "${name} is the ${role}"] 
+    value = { for name, role in var.names : upper(name) => upper(role) }
+}
